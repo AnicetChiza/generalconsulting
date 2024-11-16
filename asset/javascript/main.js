8c519518c2c3fd12403533600b90e10ae1224b6f
Anicet Chiza <chizaanicet@gmail.com>
2024-11-16 12:55:56 +0200

    add to my repository

--- asset/javascript/main.js
+++ asset/javascript/main.js
@@ -1,23 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const menuIcon = document.querySelector(".menu");
+    const menuIcon = document.querySelector(".menu-icon");
     const exitIcon = document.querySelector(".exit");
-    const overlay = document.querySelector(".overlay");
-    const menuList = document.querySelector(".header-list ul");
+    const menuList = document.querySelector(".list ul");
 
     // Ouvrir le menu
     menuIcon.addEventListener("click", () => {
         menuList.classList.add("active");
-        overlay.classList.add("active");
         exitIcon.style.display = "flex";
+        menuIcon.style.display = "none";
     });
 
     // Fermer le menu
     const closeMenu = () => {
         menuList.classList.remove("active");
-        overlay.classList.remove("active");
         exitIcon.style.display = "none";
+        menuIcon.style.display = "flex";
     };
 
     exitIcon.addEventListener("click", closeMenu);
-    overlay.addEventListener("click", closeMenu);
 });
