71498fb284448cbf7ccdfa2dbe741e7f82498851
Anicet Chiza <chizaanicet@gmail.com>
2024-11-23 11:51:36 +0200

    add to my repository

---
+++ asset/javascript/representation.js
@@ -0,0 +1,244 @@
+// Initialisation de i18next avec les traductions
+i18next.init({
+    lng: 'fr', // Langue par défaut
+    resources: {
+        "fr": {
+    "translation": {
+      "home": "Accueil",
+      "about": "A propos",
+      "status": "Status",
+      "representation-title": "Représentation",
+      "representation-description": "Chez General Consulting Group, nous mettons à profit notre expertise diversifiée pour offrir une représentation stratégique...",
+      "representation-approach": "Notre approche de représentation repose sur quelques piliers fondamentaux",
+      "market-understanding": "Compréhension approfondie du marché et de la culture locale :",
+      "market-description": "Nous travaillons à comprendre les subtilités des marchés dans lesquels nos clients souhaitent s’implanter...",
+      "communication-strategy": "Stratégie de communication et de positionnement de marque :",
+      "communication-description": "Nous élaborons des stratégies de communication qui renforcent la visibilité et la notoriété de nos clients...",
+      "government-relations": "Gestion des relations gouvernementales et institutionnelles :",
+      "government-description": "Généralement essentiel pour les entreprises en croissance, nous aidons nos clients à établir et à entretenir des relations solides...",
+      "partnership-strategy": "Stratégies de partenariat et de développement des affaires :",
+      "partnership-description": "Pour garantir un succès durable, nous identifions et facilitons les partenariats stratégiques pour nos clients...",
+      "belief-in-clients": "Nous croyons en nos clients",
+      "belief-description": "Nous croyons fermement que chaque client a un potentiel unique et que notre rôle est de maximiser ce potentiel...",
+      "we-are-your-partner": "Nous sommes votre partenaire",
+      "partner-description": "Que vous soyez une entreprise cherchant à vous lancer dans de nouveaux marchés ou à renforcer votre présence...",
+      "footer-text": "© 2024 GENERAL CONSULTING GROUP. Tous droits réservés.",
+      "footer-developed-by": "Développé par",
+      "charte-ethique": "Charte d'éthique",
+      "internal-regulations": "Réglement intérieur",
+      "general-policy": "Politique générale de charte"
+    }
+  },
+  "en": {
+    "translation": {
+      "home": "Home",
+      "about": "About",
+      "status": "Status",
+      "representation-title": "Representation",
+      "representation-description": "At General Consulting Group, we leverage our diverse expertise to provide strategic representation...",
+      "representation-approach": "Our representation approach is based on key pillars",
+      "market-understanding": "In-depth market and local culture understanding:",
+      "market-description": "We work to understand the subtleties of the markets where our clients want to establish themselves...",
+      "communication-strategy": "Communication strategy and brand positioning:",
+      "communication-description": "We develop communication strategies that enhance the visibility and recognition of our clients...",
+      "government-relations": "Management of governmental and institutional relations:",
+      "government-description": "Typically essential for growing businesses, we help our clients build and maintain strong relationships...",
+      "partnership-strategy": "Partnership strategies and business development:",
+      "partnership-description": "To ensure sustainable success, we identify and facilitate strategic partnerships for our clients...",
+      "belief-in-clients": "We believe in our clients",
+      "belief-description": "We firmly believe that every client has a unique potential, and our role is to maximize that potential...",
+      "we-are-your-partner": "We are your partner",
+      "partner-description": "Whether you are a business looking to expand into new markets or strengthen your presence...",
+      "footer-text": "© 2024 GENERAL CONSULTING GROUP. All rights reserved.",
+      "footer-developed-by": "Developed by",
+      "charte-ethique": "Ethical Charter",
+      "internal-regulations": "Internal Regulations",
+      "general-policy": "General Policy Charter"
+    }
+  },
+  "es": {
+    "translation": {
+      "home": "Inicio",
+      "about": "Acerca de",
+      "status": "Estado",
+      "representation-title": "Representación",
+      "representation-description": "En General Consulting Group, aprovechamos nuestra experiencia diversa para proporcionar representación estratégica...",
+      "representation-approach": "Nuestro enfoque de representación se basa en pilares clave",
+      "market-understanding": "Comprensión profunda del mercado y de la cultura local:",
+      "market-description": "Trabajamos para entender las sutilezas de los mercados en los que nuestros clientes desean establecerse...",
+      "communication-strategy": "Estrategia de comunicación y posicionamiento de marca:",
+      "communication-description": "Desarrollamos estrategias de comunicación que mejoran la visibilidad y el reconocimiento de nuestros clientes...",
+      "government-relations": "Gestión de relaciones gubernamentales e institucionales:",
+      "government-description": "Normalmente esencial para las empresas en crecimiento, ayudamos a nuestros clientes a construir y mantener relaciones fuertes...",
+      "partnership-strategy": "Estrategias de asociación y desarrollo de negocios:",
+      "partnership-description": "Para garantizar un éxito sostenible, identificamos y facilitamos asociaciones estratégicas para nuestros clientes...",
+      "belief-in-clients": "Creemos en nuestros clientes",
+      "belief-description": "Creemos firmemente que cada cliente tiene un potencial único, y nuestro papel es maximizar ese potencial...",
+      "we-are-your-partner": "Somos tu socio",
+      "partner-description": "Ya sea que seas una empresa que busca expandirse a nuevos mercados o fortalecer su presencia...",
+      "footer-text": "© 2024 GENERAL CONSULTING GROUP. Todos los derechos reservados.",
+      "footer-developed-by": "Desarrollado por",
+      "charte-ethique": "Carta ética",
+      "internal-regulations": "Reglamentos internos",
+      "general-policy": "Carta de políticas generales"
+    }
+  },
+  "de": {
+    "translation": {
+      "home": "Startseite",
+      "about": "Über uns",
+      "status": "Status",
+      "representation-title": "Vertretung",
+      "representation-description": "Bei General Consulting Group nutzen wir unsere vielfältige Expertise, um strategische Vertretung anzubieten...",
+      "representation-approach": "Unser Ansatz der Vertretung basiert auf einigen Schlüsselprinzipien",
+      "market-understanding": "Tiefgehendes Markt- und Kulturverständnis:",
+      "market-description": "Wir arbeiten daran, die Feinheiten der Märkte zu verstehen, in denen unsere Kunden sich etablieren möchten...",
+      "communication-strategy": "Kommunikationsstrategie und Markenpositionierung:",
+      "communication-description": "Wir entwickeln Kommunikationsstrategien, die die Sichtbarkeit und das Markenbewusstsein unserer Kunden erhöhen...",
+      "government-relations": "Management von Regierungs- und Institutsbeziehungen:",
+      "government-description": "Für wachstumsorientierte Unternehmen in der Regel unerlässlich, helfen wir unseren Kunden, starke Beziehungen aufzubauen und zu pflegen...",
+      "partnership-strategy": "Partnerschaftsstrategien und Geschäftsentwicklung:",
+      "partnership-description": "Um nachhaltigen Erfolg sicherzustellen, identifizieren und erleichtern wir strategische Partnerschaften für unsere Kunden...",
+      "belief-in-clients": "Wir glauben an unsere Kunden",
+      "belief-description": "Wir glauben fest daran, dass jeder Kunde ein einzigartiges Potenzial hat und dass es unsere Aufgabe ist, dieses Potenzial zu maximieren...",
+      "we-are-your-partner": "Wir sind Ihr Partner",
+      "partner-description": "Ob Sie ein Unternehmen sind, das in neue Märkte expandieren oder seine Präsenz stärken möchte...",
+      "footer-text": "© 2024 GENERAL CONSULTING GROUP. Alle Rechte vorbehalten.",
+      "footer-developed-by": "Entwickelt von",
+      "charte-ethique": "Ethische Charta",
+      "internal-regulations": "Interne Vorschriften",
+      "general-policy": "Allgemeine Charta der Politik"
+    }
+  },
+  ru: {
+    translation: {
+        "home": "Главная",
+        "about": "О нас",
+        "status": "Статус",
+        "representation-title": "Представительство",
+        "representation-description": "В General Consulting Group мы используем наш разнообразный опыт для предоставления стратегического представительства...",
+        "representation-approach": "Наш подход к представительству основан на нескольких ключевых столпах",
+        "market-understanding": "Глубокое понимание рынка и местной культуры:",
+        "market-description": "Мы работаем над тем, чтобы понять тонкости рынков, в которых наши клиенты хотят установить свое присутствие...",
+        "communication-strategy": "Стратегия коммуникации и позиционирования бренда:",
+        "communication-description": "Мы разрабатываем стратегии коммуникации, которые увеличивают видимость и признание наших клиентов...",
+        "government-relations": "Управление отношениями с правительством и учреждениями:",
+        "government-description": "Обычно для растущих компаний это необходимо, мы помогаем нашим клиентам строить и поддерживать крепкие отношения...",
+        "partnership-strategy": "Стратегии партнерства и развития бизнеса:",
+        "partnership-description": "Чтобы обеспечить устойчивый успех, мы определяем и содействуем стратегическим партнёрствам для наших клиентов...",
+        "belief-in-clients": "Мы верим в наших клиентов",
+        "belief-description": "Мы твердо уверены, что у каждого клиента есть уникальный потенциал, и наша задача — максимизировать этот потенциал...",
+        "we-are-your-partner": "Мы ваш партнер",
+        "partner-description": "Если вы бизнес, который хочет выйти на новые рынки или укрепить свое присутствие...",
+        "footer-text": "© 2024 GENERAL CONSULTING GROUP. Все права защищены.",
+        "footer-developed-by": "Разработано",
+        "charte-ethique": "Этический кодекс",
+        "internal-regulations": "Внутренние правила",
+        "general-policy": "Общая политика"
+        }
+    },
+    "zh": {
+        "translation": {
+        "home": "首页",
+        "about": "关于我们",
+        "status": "状态",
+        "representation-title": "代表",
+        "representation-description": "在General Consulting Group，我们利用我们的多样化专业知识提供战略代表...",
+        "representation-approach": "我们的代表方法基于几个关键支柱",
+        "market-understanding": "深入了解市场和当地文化：",
+        "market-description": "我们致力于了解我们的客户希望进入的市场的细微差别...",
+        "communication-strategy": "沟通策略和品牌定位：",
+        "communication-description": "我们制定的沟通策略提升了客户的知名度和品牌认知度...",
+        "government-relations": "政府和机构关系管理：",
+        "government-description": "对于成长中的公司来说通常是必要的，我们帮助客户建立和维护强大的关系...",
+        "partnership-strategy": "合作伙伴战略和业务发展：",
+        "partnership-description": "为了确保可持续的成功，我们为客户识别并促进战略合作伙伴关系...",
+        "belief-in-clients": "我们相信我们的客户",
+        "belief-description": "我们坚信每个客户都有独特的潜力，我们的角色是最大化这一潜力...",
+        "we-are-your-partner": "我们是您的合作伙伴",
+        "partner-description": "无论您是希望进入新市场还是加强存在的公司...",
+        "footer-text": "© 2024 GENERAL CONSULTING GROUP. 保留所有权利。",
+        "footer-developed-by": "开发者",
+        "charte-ethique": "道德章程",
+        "internal-regulations": "内部规定",
+        "general-policy": "政策总则"
+        }
+    },
+    ar: {
+    translation: {
+        "home": "الرئيسية",
+        "about": "من نحن",
+        "status": "الحالة",
+        "representation-title": "التمثيل",
+        "representation-description": "في General Consulting Group، نحن نستخدم خبرتنا المتنوعة لتقديم تمثيل استراتيجي...",
+        "representation-approach": "يستند نهجنا في التمثيل إلى بعض الركائز الرئيسية",
+        "market-understanding": "فهم عميق للسوق والثقافة المحلية:",
+        "market-description": "نحن نعمل على فهم تفاصيل الأسواق التي يرغب عملاؤنا في التواجد فيها...",
+        "communication-strategy": "استراتيجية الاتصال وتحديد الهوية العلامة التجارية:",
+        "communication-description": "نحن نطور استراتيجيات الاتصال التي تعزز من وضوح ورؤية عملائنا...",
+        "government-relations": "إدارة العلاقات الحكومية والمؤسساتية:",
+        "government-description": "عادة ما تكون ضرورية للأعمال التجارية النامية، نحن نساعد عملائنا في بناء والحفاظ على علاقات قوية...",
+        "partnership-strategy": "استراتيجيات الشراكة وتطوير الأعمال:",
+        "partnership-description": "لضمان النجاح المستدام، نقوم بتحديد وتسهيل الشراكات الاستراتيجية لعملائنا...",
+        "belief-in-clients": "نحن نؤمن بعملائنا",
+        "belief-description": "نؤمن إيمانًا راسخًا أن كل عميل لديه إمكانيات فريدة، ودورنا هو تعزيز تلك الإمكانيات...",
+        "we-are-your-partner": "نحن شريكك",
+        "partner-description": "سواء كنت شركة تتطلع للتوسع في أسواق جديدة أو تعزيز وجودك...",
+        "footer-text": "© 2024 GENERAL CONSULTING GROUP. جميع الحقوق محفوظة.",
+        "footer-developed-by": "تم التطوير بواسطة",
+        "charte-ethique": "الميثاق الأخلاقي",
+        "internal-regulations": "اللوائح الداخلية",
+        "general-policy": "الميثاق السياسي العام"
+        }
+    },
+    "pt": {
+    "translation": {
+        "home": "Início",
+        "about": "Sobre",
+        "status": "Status",
+        "representation-title": "Representação",
+        "representation-description": "Na General Consulting Group, aproveitamos nossa experiência diversificada para fornecer representação estratégica...",
+        "representation-approach": "Nossa abordagem de representação é baseada em pilares fundamentais",
+        "market-understanding": "Compreensão profunda do mercado e da cultura local:",
+        "market-description": "Trabalhamos para entender as sutilezas dos mercados onde nossos clientes desejam se estabelecer...",
+        "communication-strategy": "Estratégia de comunicação e posicionamento de marca:",
+        "communication-description": "Desenvolvemos estratégias de comunicação que aumentam a visibilidade e o reconhecimento dos nossos clientes...",
+        "government-relations": "Gestão de relações governamentais e institucionais:",
+        "government-description": "Normalmente essencial para empresas em crescimento, ajudamos nossos clientes a construir e manter relacionamentos sólidos...",
+        "partnership-strategy": "Estratégias de parcerias e desenvolvimento de negócios:",
+        "partnership-description": "Para garantir o sucesso sustentável, identificamos e facilitamos parcerias estratégicas para nossos clientes...",
+        "belief-in-clients": "Acreditamos nos nossos clientes",
+        "belief-description": "Acreditamos firmemente que cada cliente tem um potencial único e que nosso papel é maximizar esse potencial...",
+        "we-are-your-partner": "Somos seu parceiro",
+        "partner-description": "Se você é uma empresa procurando expandir para novos mercados ou fortalecer sua presença...",
+        "footer-text": "© 2024 GENERAL CONSULTING GROUP. Todos os direitos reservados.",
+        "footer-developed-by": "Desenvolvido por",
+        "charte-ethique": "Carta ética",
+        "internal-regulations": "Regulamentos internos",
+        "general-policy": "Carta de políticas gerais"
+        },
+    }
+        // Ajoutez d'autres langues ici si nécessaire
+    }
+}, function(err, t) {
+    if (err) return console.error(err);
+
+    // Mettre à jour les éléments après l'initialisation
+    updateContent();
+});
+
+// Fonction pour mettre à jour le contenu
+function updateContent() {
+    document.querySelectorAll('[data-i18n]').forEach(function(element) {
+        const key = element.getAttribute('data-i18n');
+        element.innerHTML = i18next.t(key);
+    });
+}
+
+// Fonction pour changer la langue
+document.getElementById('language-select').addEventListener('change', function(event) {
+    const selectedLanguage = event.target.value;
+    i18next.changeLanguage(selectedLanguage, function() {
+        updateContent();
+    });
+});
